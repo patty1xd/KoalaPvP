053ec43a8cf755507c30a1c26ace6271dd91e957
patty1xd <alexandruflorea2019@gmail.com>
2026-05-25 21:51:54 +0300

    Add files via upload

--- src/main/java/dev/koala/koalapvp/KoalaPvP.java
+++ src/main/java/dev/koala/koalapvp/KoalaPvP.java
@@ -1,17 +1,30 @@
 package dev.koala.koalapvp;
 
+import com.github.retrooper.packetevents.PacketEvents;
+import com.github.retrooper.packetevents.event.PacketListenerPriority;
 import dev.koala.koalapvp.command.KoalaPvPCommand;
 import dev.koala.koalapvp.config.KoalaConfig;
+import dev.koala.koalapvp.knockback.ClientGroundTracker;
 import dev.koala.koalapvp.knockback.KnockbackEngine;
 import dev.koala.koalapvp.listener.CombatListener;
 import dev.koala.koalapvp.util.Logger;
+import io.github.retrooper.packetevents.factory.spigot.SpigotPacketEventsBuilder;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class KoalaPvP extends JavaPlugin {
 
     private static KoalaPvP instance;
-    private KoalaConfig koalaConfig;
-    private KnockbackEngine knockbackEngine;
+    private KoalaConfig         koalaConfig;
+    private ClientGroundTracker groundTracker;
+    private KnockbackEngine     knockbackEngine;
+
+    @Override
+    public void onLoad() {
+        // PacketEvents requires setAPI + load() during onLoad so packet
+        // pipelines are wired in before any player joins.
+        PacketEvents.setAPI(SpigotPacketEventsBuilder.build(this));
+        PacketEvents.getAPI().load();
+    }
 
     @Override
     public void onEnable() {
@@ -21,9 +34,17 @@ public final class KoalaPvP extends JavaPlugin {
         koalaConfig = new KoalaConfig(this);
         koalaConfig.load();
 
-        knockbackEngine = new KnockbackEngine(this);
+        groundTracker = new ClientGroundTracker(this);
+        knockbackEngine = new KnockbackEngine(this, groundTracker);
 
+        // Connect the packet listener (reads client onGround flags off netty)
+        PacketEvents.getAPI().getEventManager()
+                .registerListener(groundTracker, PacketListenerPriority.NORMAL);
+        PacketEvents.getAPI().init();
+
+        // Bukkit listeners
         getServer().getPluginManager().registerEvents(new CombatListener(this), this);
+        getServer().getPluginManager().registerEvents(groundTracker, this);
 
         KoalaPvPCommand cmd = new KoalaPvPCommand(this);
         getCommand("koalapvp").setExecutor(cmd);
@@ -31,15 +52,21 @@ public final class KoalaPvP extends JavaPlugin {
 
         Logger.info("&aKoalaPvP &7v" + getDescription().getVersion() + " &aenabled.");
         Logger.info("&7Ping compensation: &e" + koalaConfig.isPingCompEnabled()
-                + " &7| ground-trust-ticks: &e" + koalaConfig.getGroundTrustMaxTicks());
+                + " &7| ground-trust-ticks: &e" + koalaConfig.getGroundTrustMaxTicks()
+                + " &7| client-state via PacketEvents: &aon");
     }
 
     @Override
     public void onDisable() {
+        try {
+            PacketEvents.getAPI().terminate();
+        } catch (Throwable ignored) {}
+        if (groundTracker != null) groundTracker.clear();
         Logger.info("&cKoalaPvP disabled.");
     }
 
-    public static KoalaPvP getInstance()          { return instance; }
-    public KoalaConfig     getKoalaConfig()        { return koalaConfig; }
-    public KnockbackEngine getKnockbackEngine()    { return knockbackEngine; }
+    public static KoalaPvP getInstance()        { return instance; }
+    public KoalaConfig     getKoalaConfig()     { return koalaConfig; }
+    public KnockbackEngine getKnockbackEngine() { return knockbackEngine; }
+    public ClientGroundTracker getGroundTracker() { return groundTracker; }
 }
